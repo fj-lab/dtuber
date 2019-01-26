676d14ddd128d0fd0926330998bad66437e53c6a
baibai25 <kamuy.camel.0225@gmail.com>
2019-01-26 23:07:13 +0900

    First commit

---
+++ pycatfd/augment.py
@@ -0,0 +1,20 @@
+import glob
+import os
+import cv2
+
+
+def main():
+
+    os.makedirs('fliped',exist_ok=True)
+    img_list = glob.glob('./image_dir/*.jpg')
+
+    # Flip images and save
+    for path in img_list:
+        name, ext = os.path.splitext(os.path.basename(path))
+        img = cv2.imread(path, cv2.IMREAD_COLOR)
+        flip = cv2.flip(img, 1)
+        cv2.imwrite('./fliped/fliped_{}.jpg'.format(name), flip)
+
+
+if __name__ == "__main__":
+    main()
