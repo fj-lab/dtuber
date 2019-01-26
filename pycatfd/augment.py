4fed16b68920d17a0b5b2b79d7066f57e623263e
baibai25 <kamuy.camel.0225@gmail.com>
2019-01-26 23:23:19 +0900

    Fix save path

--- pycatfd/augment.py
+++ pycatfd/augment.py
@@ -5,7 +5,7 @@ import cv2
 
 def main():
 
-    os.makedirs('fliped',exist_ok=True)
+    #os.makedirs('fliped',exist_ok=True)
     img_list = glob.glob('./image_dir/*.jpg')
 
     # Flip images and save
@@ -13,7 +13,7 @@ def main():
         name, ext = os.path.splitext(os.path.basename(path))
         img = cv2.imread(path, cv2.IMREAD_COLOR)
         flip = cv2.flip(img, 1)
-        cv2.imwrite('./fliped/fliped_{}.jpg'.format(name), flip)
+        cv2.imwrite('./image_dir/fliped_{}.jpg'.format(name), flip)
 
 
 if __name__ == "__main__":
